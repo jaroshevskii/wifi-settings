12639ee8ebb299bc5d11119a1f507839622dd3da
Sasha Jaroshevskii 🦄 <72662383+jaroshvekii@users.noreply.github.com>
2025-10-30 19:47:04 +0100

    Initial Commit

---
+++ WiFiSettings/WiFiSettingsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WiFiSettingsApp.swift
+//  WiFiSettings
+//
+//  Created by Sasha Jaroshevskii on 10/30/25.
+//
+
+import SwiftUI
+
+@main
+struct WiFiSettingsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ WiFiSettingsTests/WiFiSettingsTests.swift
@@ -0,0 +1,17 @@
+//
+//  WiFiSettingsTests.swift
+//  WiFiSettingsTests
+//
+//  Created by Sasha Jaroshevskii on 10/30/25.
+//
+
+import Testing
+@testable import WiFiSettings
+
+struct WiFiSettingsTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
